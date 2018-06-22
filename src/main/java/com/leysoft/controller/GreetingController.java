ab097e6c5c0fc711cd968429454e9823cc9bc587
will.leyton <will.leyton@valid.com>
2018-06-22 15:51:25 -0500

    Added dto's

--- src/main/java/com/leysoft/controller/GreetingController.java
+++ src/main/java/com/leysoft/controller/GreetingController.java
@@ -4,12 +4,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.leysoft.dto.GreetingRequest;
+import com.leysoft.dto.GreetingResponse;
+
 @RestController
 @RequestMapping(value = {"/greeting"})
 public class GreetingController {
 	
 	@PostMapping
-	public String greeting(String name) {
-		return "Hello " + name;
+	public GreetingResponse greeting(GreetingRequest request) {
+		GreetingResponse response = new GreetingResponse();
+		response.setMessage("Hello " + request.getName());
+		return response;
 	}
 }
---
+++ src/main/java/com/leysoft/dto/GreetingResponse.java
@@ -0,0 +1,14 @@
+package com.leysoft.dto;
+
+public class GreetingResponse {
+	
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
